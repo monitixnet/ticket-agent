3beb15a92454fa26bedcc75de871ccf595b0612e
Nathan Yeung <info@monitix.net>
2026-08-19 13:03:37 -0700

    Updating migrations to have correct table name etc

--- migrations/0023_limit_segerstrom_discovery_to_segerstrom_hall.sql
+++ migrations/0023_limit_segerstrom_discovery_to_segerstrom_hall.sql
@@ -1,4 +1,4 @@
 -- Initial production rollout: persist and monitor Segerstrom Hall only.
-UPDATE venue_runtime_config
+UPDATE venue_runtime_configs
 SET config_json = json_set(COALESCE(config_json, '{}'), '$.discoveryAllowedHalls', json_array('Segerstrom Hall'))
 WHERE venue_id = 'segerstrom_center';
